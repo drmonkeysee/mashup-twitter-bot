60f9e5d6b353ec3f3874bd30effafc8dcf699f93
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-12-14 19:24:16 -0800

    reorganize code into tweet bot instance using events from store and callback through users service

--- server.js
+++ server.js
@@ -1,59 +1,22 @@
 var path = require('path'),
     Twit = require('twit'),
     users = require('./lib/users'),
-    tweetStore = require('./lib/twitter-store'),
+    tweetStore = require('./lib/tweet-store'),
     tweetBot = require('./lib/tweetbot'),
     credentials = require('./config/credentials.json');
 
 var twitterService = new Twit(credentials);
 
 var store = tweetStore.create(path.join(__dirname, 'data/store'));
-store
-    .on('init', function (s) { /* fetch initial user history and store */ })
-    .on('update', function (s) { /* initialize tweeter */ })
-    .load(function (s) { /* set up user updates and tweet schedule */ });
-
-var bot = new tweetBot.Bot(store);
-
-bot.tweet();
-
-var usersLoader = users(twitterApi);
-
-var userDataFile = path.join(__dirname, 'data/users.json');
-usersLoader.load(userDataFile, function (err, users) {
+var userService = users.create(path.join(__dirname, 'data/users.json'), store, twitterService);
+var bot = tweetBot.create(store, userService, twitterService);
+bot.start(function (err) {
+    console.log('invoke scheduler');
     if (err) {
-        console.log(err);
-    }
-
-    var userCount = users.length;
-    var finishedUsers = 0;
-    for (var i = 0; i < userCount; ++i) {
-        users[i].loadTweets(function processTweets(err, user) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            if (user.tweets.length > 0) {
-                try {
-                    store.saveSync(user.tweets);
-                    console.log('Saved %d tweets for %s.', user.tweets.length, user.name);
-                } catch (ex) {
-                    console.log('Error saving tweets for %s: %j', user.name, ex);
-                    return;
-                }
-                user.loadTweets(processTweets);
-            } else if (++finishedUsers == userCount) {
-                store.commit(function (err) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        bot.tweet();
-                    }
-                });
-            }
-
-            //applyToGenerator(user.tweets, userCount);
-        });
+        console.log('Startup error: %j', err);
+    } else {
+        setInterval(function () {
+            bot.tweet();
+        }, 5000);
     }
 });
